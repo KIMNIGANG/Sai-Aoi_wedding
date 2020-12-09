9cdee54536755e3ecfc655473ba8d0c3fc04ff1e
kimnigang <zeus2424797@gmail.com>
2020-12-09 18:07:06 +0900

    map done

--- main.js
+++ main.js
@@ -2,8 +2,23 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 25.101327, lng: 121.5455816 },
-    zoom: 13,
+    zoom: 15,
   });
+
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.BOUNCE,
+    position: { lat: 25.101327, lng: 121.5455816 },
+  });
+  marker.addeveListner("click", toggleBounce);
+}
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 
 let counter = 1;
